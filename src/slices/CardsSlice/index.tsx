7723d507be5c778378801c21de40107221c394e0
MelnykovSV <svmelnykov@gmail.com>
2024-07-26 14:39:07 +0300

    styled cards

--- src/slices/CardsSlice/index.tsx
+++ src/slices/CardsSlice/index.tsx
@@ -11,29 +11,46 @@ export type CardsSliceProps = SliceComponentProps<Content.CardsSliceSlice>;
  * Component for "CardsSlice" Slices.
  */
 const CardsSlice = ({ slice }: any): JSX.Element => {
-  console.log("slice");
-  console.log(slice);
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className={s.wrapper}>
+      className={s.wrapper}
+      style={{
+        "--columns": slice.primary.columns,
+        "--alignTitle": slice.primary.align_title,
+        "--alignTitleMobile": slice.primary.align_title,
+        "--alignSubtitle": slice.primary.align_title,
+        "--alignSubtitleMobile": slice.primary.align_title,
+        "--alignText": slice.primary.align_title,
+        "--alignTextMobile": slice.primary.align_title,
+      }}>
       <div className={s.container}>
-        <h2>{slice.primary.title}</h2>
-        <p>{slice.primary.subtitle}</p>
-        <PrismicRichText field={slice.primary.text} />
-      </div>
+        {slice.primary.title && (
+          <h2 className={s.title}>{slice.primary.title}</h2>
+        )}
+        {slice.primary.subtitle && (
+          <p className={s.subtitle}>{slice.primary.subtitle}</p>
+        )}
+        {slice.primary.text && (
+          <div className={s.text}>
+            <PrismicRichText field={slice.primary.text} />
+          </div>
+        )}
 
-      <ul>
-        {/* <p>{JSON.stringify(slice.primary.cards[0])}</p> */}
-        {slice.primary.cards.map((item: any, i: any) => (
-          // Render the item
-          <li key={i}>
-            <h3>{item.title}</h3>
-            <PrismicRichText field={item.text} />
-          </li>
-        ))}
-      </ul>
+        {slice.primary.cards.length && (
+          <ul className={s.cardsList}>
+            {/* <p>{JSON.stringify(slice.primary.cards[0])}</p> */}
+            {slice.primary.cards.map((item: any, i: any) => (
+              // Render the item
+              <li key={i}>
+                <h3 className={s.cardTitle}>{item.title}</h3>
+                <PrismicRichText field={item.text} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </section>
   );
 };
